0a66f61826bac9edf7e4d9d5374ef769fce19127
Kwong How <kwonghow@gmail.com>
2016-02-02 12:34:13 +0800

    Add more config for Bootstrap and SCSS loading

--- webpack.config.js
+++ webpack.config.js
@@ -34,19 +34,45 @@ module.exports = {
     })
   ],
   module: {
-    loaders: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.json?$/,
-      loader: 'json'
-    }, {
-      test: /\.css$/,
-      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
-    }, {
-      test   : /\.woff|\.woff2|\.svg|.eot|\.ttf/,
-      loader : 'url?prefix=font/&limit=10000'
-    }]
+    loaders: [
+      // Convert SCSS into CSS with autoprefixer
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style',
+          'css',
+          'autoprefixer?browsers=last 2 versions',
+          'resolve-url',
+          'sass?sourceMap&outputStyle=expanded'
+        ]
+      },
+      // Font/svg loaders
+      {
+        test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      },
+      // Transpile other JS/JSX files back to ES5
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|__tests__)/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ],
+    sassLoader: {
+      includePaths: [
+        path.resolve(__dirname, '../')
+      ]
+    }
   }
 };
