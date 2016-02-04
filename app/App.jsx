c07acf45e3a73c06c044b676abb6327a0d7748fe
Kwong How <kwonghow@gmail.com>
2016-02-04 20:26:59 +0800

    Use vanity LinkedIn url

--- app/App.jsx
+++ app/App.jsx
@@ -138,7 +138,7 @@ class App extends Component {
         </aside>
         <footer className="footer">
           <p>
-            Built in React by <a href="https://sg.linkedin.com/in/kwong-how-ong-85131422" target="_blank">Kwong How</a><br />
+            Built in React by <a href="https://sg.linkedin.com/in/kwonghow" target="_blank">Kwong How</a><br />
             Last updated on 4 February 2016
           </p>
         </footer>
